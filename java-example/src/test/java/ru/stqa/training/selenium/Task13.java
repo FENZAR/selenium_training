ba7296eb0822398eb1b8b3f8aab60e3dcc9e7beb
FenZar <regfen@gmail.com>
2017-06-26 08:11:50 +0700

    Task13

--- java-example/src/test/java/ru/stqa/training/selenium/Task13.java
+++ java-example/src/test/java/ru/stqa/training/selenium/Task13.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.NoSuchElementException;
+import org.openqa.selenium.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class Task13 {
@@ -21,18 +21,18 @@ public class Task13 {
     @Before
     public  void start() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("http://localhost/litecart/en/");
     }
-/*
+
     @After
     public void stop() {
         driver.quit();
         driver = null;
     }
-*/
 
-    public boolean isElementPresent(WebDriver driver, By locator) {
+
+    private boolean isElementPresent(WebDriver driver, By locator) {
         try {
             driver.findElement(locator);
             return true;
@@ -50,27 +50,35 @@ public class Task13 {
 
     @Test
     public void Task13() {
-        WebElement e;
-        Integer cnt;
-        By locator;
+        WebElement element;
+        By locator, locatorW;
+        String DuckName;
         WebDriverWait wait = new WebDriverWait(driver, 5/*seconds*/);
 
         for (int i = 0; i < 3; i++) {
             driver.findElement(By.cssSelector("div#box-most-popular a:first-child")).click();
-            e = driver.findElement(By.cssSelector("div#cart > a.content > span.quantity"));
+            element = driver.findElement(By.cssSelector("div#cart > a.content > span.quantity"));
 
             locator = By.cssSelector("select[name = 'options[Size]']");
-            Assert.assertTrue(isElementPresent(driver, locator));
-            //if(isElementPresent(driver, locator)) {
-            //    selectThis(driver, locator, 1);
-            //}
+            if(isElementPresent(driver, locator)) {
+                selectThis(driver, locator, 1);
+            }
             driver.findElement(By.cssSelector("button[name = 'add_cart_product']")).click();
-           // wait.until(ExpectedConditions.textToBePresentInElement(e, Integer.toString(i + 1)));
+            wait.until(ExpectedConditions.textToBePresentInElement(element, Integer.toString(i + 1)));
 
             driver.findElement(By.cssSelector("div#site-menu-wrapper a")).click();
         }
 
-        //driver.findElement(By.xpath("//a[contains(text(), 'Checkout »')]")).click();
+        driver.findElement(By.xpath("//a[contains(text(), 'Checkout »')]")).click();
 
+        // Remove
+        locatorW = By.cssSelector("form[name = 'cart_form'] > div a > strong");
+        while (isElementPresent(driver, locatorW)) {
+            DuckName = driver.findElement(locatorW).getText();
+            locator = By.xpath("//table[@class = 'dataTable rounded-corners']//td[contains(text(), '" + DuckName + "')]");
+            element = driver.findElement(locator);
+            driver.findElement(By.cssSelector("button[name = 'remove_cart_item']")).click();
+            wait.until(ExpectedConditions.stalenessOf(element));
+        }
     }
 }
